bcd1003310d6775dbb0cc0283b502fed0d8c52bc
suulcoder <ef.proyectomundial@gmail.com>
2019-03-08 00:25:41 -0600

    a GUI with a lot of bugs, i mean a hive of bugs

---
+++ src/com/company/DoublyLinkedNode.java
@@ -0,0 +1,60 @@
+package com.company;
+
+/**
+ * Node used on the double linked list
+ * @param <E>
+ */
+public class DoublyLinkedNode<E> {
+    protected E data;
+    protected DoublyLinkedNode<E> nextElement;
+    protected DoublyLinkedNode<E> previousElement;
+
+    /**
+     * Constructor of a double linked node
+     * @param v type of element the node contains
+     * @param next the node it points to
+     * @param previous the node that points to this node
+     */
+    public DoublyLinkedNode(E v,
+                            DoublyLinkedNode<E> next,
+                            DoublyLinkedNode<E> previous) {
+        data = v;
+        nextElement = next;
+        if (nextElement != null)
+            nextElement.previousElement = this;
+        previousElement = previous;
+        if (previousElement != null)
+            previousElement.nextElement = this;
+    }
+
+    public DoublyLinkedNode(E v)
+// post: constructs a single element
+    {
+        this(v, null, null);
+    }
+
+    /**
+     * Sets the pointer of the node
+     * @param objeto the value of the next pointer
+     */
+    public void setNext(DoublyLinkedNode<E> objeto) {
+        this.nextElement = objeto;
+    }
+
+    /**
+     * Method used to get the previous node
+     * @return the previous node
+     */
+    public DoublyLinkedNode<E> getPreviousElement(){
+        return (previousElement);
+    }
+
+    /**
+     * Used to get the data of the current node
+     * @return the object the node contains
+     */
+    public E getData(){
+        return  data;
+    }
+
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,30 +13,82 @@ import java.util.Map;
 import java.util.function.BinaryOperator;
 import java.util.function.Function;
 import java.util.function.UnaryOperator;
+import java.awt.event.*;
+import java.awt.*;
+import javax.swing.*;
 
-public class Main {
+public class Main extends JFrame implements ActionListener{
+
+    static JButton botton, reset;
+    static JTextField textField = new JTextField(20);
+    private Panel panelEntrada, panelSalida;
+    private JPanel panelDeLaVentana;
+    private JTextArea salida = new JTextArea();
+
+    public Main(){
+        super("LISP-INTERPRETER");/*Sera el nombre de la ventana*/
+        botton = new JButton("Empezar");
+        reset = new JButton("RESET");
+        botton.setActionCommand("empezar");
+        botton.addActionListener(this);
+        textField.addActionListener(this);
+        panelDeLaVentana = (JPanel)this.getContentPane();
+        panelEntrada = new Panel();//los siguientes paneles son para poner orden y estetica
+        panelSalida = new Panel();
+        panelEntrada.add(botton);
+        panelEntrada.add(reset);
+        panelSalida.add(textField);
+        panelSalida.add(salida);
+        panelDeLaVentana.add(panelEntrada,BorderLayout.NORTH);//agreamos las ventanas a la interfaz grafica
+        panelDeLaVentana.add(panelSalida,BorderLayout.SOUTH);
+    }
+
+    public static void main(String[] args) {
+        /*Imprimimos la ventana en la pantalla*/
+
+        Main miAplicacion = new Main();
+        miAplicacion.setBounds(10,10,200,200);
+        miAplicacion.pack();
+        miAplicacion.setVisible(true);
+    }
 
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if("empezar".equals((e.getActionCommand()))){
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        Interpreter interprete = new Interpreter();
+            Interpreter interprete = new Interpreter();
 
-        interprete.getGlobal().put("pi", Math.PI);
-        interprete.getGlobal().put("+", (BinaryOperator<Number>) Interpreter::add);//Agregamos estas funciones a nuestro map
-        interprete.getGlobal().put("*", (BinaryOperator<Number>) Interpreter::multiply);
-        interprete.getGlobal().put("abs", (UnaryOperator<Number>) Interpreter::abs);
+            interprete.getGlobal().put("pi", Math.PI);
+            interprete.getGlobal().put("+", (BinaryOperator<Number>) Interpreter::add);//Agregamos estas funciones a nuestro map
+            interprete.getGlobal().put("*", (BinaryOperator<Number>) Interpreter::multiply);
+            interprete.getGlobal().put("abs", (UnaryOperator<Number>) Interpreter::abs);
 
-        while (true) {
-            System.out.print(">>>");
-            String input = br.readLine();
-            if (input == null || input.equals("quit")) break;
-            try {
-                System.out.println(Arrays.toString(Interpreter.reemplazar(input)));
-                System.out.println(Interpreter.leer(input));
-                System.out.println(Interpreter.evaluate(Interpreter.leer(input)));
-            } catch (Exception e) {
-                e.printStackTrace();
+            while (true) {
+                System.out.print(">>>");
+                String input = textField.getText();
+                System.out.println(input);
+                try {
+                    input = br.readLine();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+                if (input == null || input.equals("quit")) break;
+                try {
+                    String retorno = " ";
+                    retorno = retorno + Arrays.toString(Interpreter.reemplazar(input)) + "\n";
+                    retorno = retorno + Interpreter.leer(input) + "\n";
+                    retorno = retorno + Interpreter.evaluate(Interpreter.leer(input)) + "\n";
+                    salida.setText(retorno);
+                } catch (Exception f) {
+                    f.printStackTrace();
+                }
+            }
+        }
+        else{
+            if("RESET".equals((e.getActionCommand()))){
+                salida.setText(" ");
             }
         }
     }
