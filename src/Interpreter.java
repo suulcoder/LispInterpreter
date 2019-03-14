bf6c5acf0098a8f3720a52bc7b41501c64ad578e
SuulCoder <35270907+suulcoder@users.noreply.github.com>
2019-03-14 08:15:54 -0600

    Update Interpreter.java

--- src/Interpreter.java
+++ src/Interpreter.java
@@ -11,7 +11,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.BinaryOperator;
-import java.util.function.Function;
 import java.util.function.UnaryOperator;
 
 import static java.util.logging.Level.parse;
