de1f293338ec7bcf993abb28939a9996c94cee26
RobertoFigueroa <35494933+RobertoFigueroa@users.noreply.github.com>
2019-03-13 20:45:08 -0600

    Inicio de la interfaz gráfica

---
+++ src/LispInterpreterGUI.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class LispInterpreterGUI {
+    private JPanel panelMain;
+    private JTextArea textPathDirectory;
+    private JButton interpreterButton;
+    private JTextArea textInterpreter;
+
+    public LispInterpreterGUI(){
+        // TODO: 13/03/2019 Add functions to the button and to the JText
+    }
+}
