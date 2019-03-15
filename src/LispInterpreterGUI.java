9c8ae0446b3e942067483410aa177c443a1be4e0
RobertoFigueroa <35494933+RobertoFigueroa@users.noreply.github.com>
2019-03-14 19:39:51 -0600

    Modificacion de la clase main y arreglo del git ya que no me dejaba hacer Pull ni Push

--- src/LispInterpreterGUI.java
+++ src/LispInterpreterGUI.java
@@ -1,12 +1,99 @@
-import javax.swing.*;
+import com.sun.istack.internal.Nullable;
 
-public class LispInterpreterGUI {
-    private JPanel panelMain;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.function.BinaryOperator;
+import java.util.function.UnaryOperator;
+import java.util.stream.Stream;
+
+public class LispInterpreterGUI extends JFrame  {
+    public JPanel panelMain;
     private JTextArea textPathDirectory;
     private JButton interpreterButton;
-    private JTextArea textInterpreter;
+    private JTextArea outPutInterpreter;
+    private JButton loadPathButton;
 
-    public LispInterpreterGUI(){
-        // TODO: 13/03/2019 Add functions to the button and to the JText
+
+    public LispInterpreterGUI() {
+        //when the button is clicked the program would star reading the lisp code on the JText area
+
+
+        loadPathButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+                try {readTheRawCode();}
+                catch (IOException error){
+                    error.printStackTrace();
+                }
+            }
+        });
+
+        //Button to start the interpretation
+        interpreterButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                Interpreter interprete = new Interpreter();
+                interprete.getGlobal().put("pi", Math.PI);
+                interprete.getGlobal().put("+", (BinaryOperator<Number>) Interpreter::add);//Agregamos estas funciones a nuestro map
+                interprete.getGlobal().put("*", (BinaryOperator<Number>) Interpreter::multiply);
+                interprete.getGlobal().put("abs", (UnaryOperator<Number>) Interpreter::abs);
+                interprete.getGlobal().put("-", (BinaryOperator<Number>) Interpreter::subtract);
+                interprete.getGlobal().put("/", (BinaryOperator<Number>) Interpreter::divide);
+                final Integer[] lineCounter = {1};
+
+                try {
+                    Stream<String> lines = Files.lines(
+                            Paths.get("datos.txt"),
+                            StandardCharsets.UTF_8
+                    );
+                    lines.forEach(s -> {
+                        if (s == null || s.equals("quit"))
+                            try {
+                                String retorno = " ";
+                                retorno = retorno + Arrays.toString(Interpreter.reemplazar(s)) + "\n";
+                                retorno = retorno + Interpreter.leer(s) + "\n";
+                                System.out.println(retorno);
+                                retorno = retorno + Interpreter.evaluate(Interpreter.leer(s)) + "\n";
+                                outPutInterpreter.setText(retorno);
+                                System.out.println(retorno);
+                                lineCounter[0] += 1;
+
+                            } catch (Exception f) {
+                                f.printStackTrace();
+                                System.out.println("Error en linea:" + lineCounter[0]);
+                            }
+
+                    });
+                } catch (IOException exception) {
+                    outPutInterpreter.setText("Error");
+
+                }
+
+
+                if ("RESET".equals((e.getActionCommand()))) {
+                    outPutInterpreter.setText(" ");
+                }
+
+            }
+
+        });
     }
+    //method for read the code an write it on a local text file
+    private void readTheRawCode() throws IOException {
+        String rawCode = textPathDirectory.getText();
+        BufferedWriter writer = new BufferedWriter(new FileWriter("C:\\Users\\rober\\OneDrive\\Documentos\\GitHub\\LispInterpreter\\src\\program_txt"));
+        writer.write(rawCode);
+        writer.close();
+    }
+
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,108 +1,25 @@
 
-
+// Esta clase se utilizó para pruebas ahora la clase Driver es LispInterpreterGUI ya que contiene la interfaz grafica
 import javafx.scene.AmbientLight;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.function.BinaryOperator;
-import java.util.function.Function;
-import java.util.function.UnaryOperator;
-import java.awt.event.*;
-import java.awt.*;
-import java.util.stream.Stream;
 import javax.swing.*;
+public class Main{
+public static void main(String[]args){
 
-public class Main extends JFrame implements ActionListener{
+        //inicialize the environment of the GUI for display all the buttons and texts fields
+        SwingUtilities.invokeLater(new Runnable() {
+                @Override
+                public void run() {
+                        JFrame frame=new JFrame("panelMain");
+                        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                        frame.setContentPane(new LispInterpreterGUI().panelMain);
+                        frame.setSize(600,600);
+                        frame.pack();
+                        frame.setVisible(true);
 
-    static JButton botton, reset;
-    static JTextField textField = new JTextField(20);
-    private Panel panelEntrada, panelSalida;
-    private JPanel panelDeLaVentana;
-    private JTextArea salida = new JTextArea();
-
-    public Main(){
-        super("LISP-INTERPRETER");/*Sera el nombre de la ventana*/
-        botton = new JButton("Empezar");
-        reset = new JButton("RESET");
-        botton.setActionCommand("empezar");
-        botton.addActionListener(this);
-        textField.addActionListener(this);
-        panelDeLaVentana = (JPanel)this.getContentPane();
-        panelEntrada = new Panel();//los siguientes paneles son para poner orden y estetica
-        panelSalida = new Panel();
-        panelEntrada.add(botton);
-        panelEntrada.add(reset);
-        panelSalida.add(textField);
-        panelSalida.add(salida);
-        panelDeLaVentana.add(panelEntrada,BorderLayout.NORTH);//agreamos las ventanas a la interfaz grafica
-        panelDeLaVentana.add(panelSalida,BorderLayout.SOUTH);
-    }
-
-    public static void main(String[] args) {
-        /*Imprimimos la ventana en la pantalla*/
-
-        Main miAplicacion = new Main();
-        miAplicacion.setBounds(10,10,200,200);
-        miAplicacion.pack();
-        miAplicacion.setVisible(true);
-    }
+                }
+        });
 
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        if("empezar".equals((e.getActionCommand()))) {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-            Interpreter interprete = new Interpreter();
-
-            interprete.getGlobal().put("pi", Math.PI);
-            interprete.getGlobal().put("+", (BinaryOperator<Number>) Interpreter::add);//Agregamos estas funciones a nuestro map
-            interprete.getGlobal().put("*", (BinaryOperator<Number>) Interpreter::multiply);
-            interprete.getGlobal().put("abs", (UnaryOperator<Number>) Interpreter::abs);
-            interprete.getGlobal().put("-", (BinaryOperator<Number>) Interpreter::subtract);
-            interprete.getGlobal().put("/", (BinaryOperator<Number>) Interpreter::divide);
-            final Integer[] lineCounter = {1};
-
-            try {
-                Stream<String> lines = Files.lines(
-                        Paths.get("datos.txt"),
-                        StandardCharsets.UTF_8
-                );
-                lines.forEach(s -> {
-                    if (s == null || s.equals("quit"))
-                        try {
-                            String retorno = " ";
-                            retorno = retorno + Arrays.toString(Interpreter.reemplazar(s)) + "\n";
-                            retorno = retorno + Interpreter.leer(s) + "\n";
-                            System.out.println(retorno);
-                            retorno = retorno + Interpreter.evaluate(Interpreter.leer(s)) + "\n";
-                            salida.setText(retorno);
-                            System.out.println(retorno);
-                            lineCounter[0] += 1;
-
-                        } catch (Exception f) {
-                            f.printStackTrace();
-                            System.out.println("Error en linea:" + lineCounter[0]);
-                        }
-
-                });
-            } catch (IOException exception) {
-                System.out.println("Error");
-            }
         }
-        else{
-            if("RESET".equals((e.getActionCommand()))){
-                salida.setText(" ");
-            }
         }
-    }
-}
